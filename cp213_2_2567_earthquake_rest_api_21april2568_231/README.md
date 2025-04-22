5f2c9c0b27df5499af5dab6acb62e5347ef46c02
Script_Shadow <kongpob.gafe@g.swu.ac.th>
2025-04-23 00:20:53 +0700

    Delete cp213_2_2567_earthquake_rest_api_21april2568_231 directory

--- cp213_2_2567_earthquake_rest_api_21april2568_231/README.md
+++
@@ -1,3 +0,0 @@
-# cp213_2_2567_earthquake_rest_api_21april2568_231
-
-A new Flutter project.
--- cp213_2_2567_earthquake_rest_api_21april2568_231/android/app/src/main/kotlin/com/example/cp213_2_2567_earthquake_rest_api_21april2568_231/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cp213_2_2567_earthquake_rest_api_21april2568_231
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
